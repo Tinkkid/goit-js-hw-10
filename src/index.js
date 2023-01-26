db596abd1642593d6bf4efdde80647d24a6a997f
Yuliia Kostovynska <y.kostovynska@gmail.com>
2023-01-26 13:42:55 +0100

    finished task

--- src/index.js
+++ src/index.js
@@ -1,34 +1,18 @@
 import './css/styles.css';
+import getRefs from './js/refs';
+import API from './js/api-service-country';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
 import 'notiflix/dist/notiflix-3.2.6.min.css';
 
 
 const DEBOUNCE_DELAY = 300;
-const refs = {
-  inputCountry: document.querySelector('#search-box'),
-  countryList: document.querySelector('.country-list'),
-  countryInfo: document.querySelector('.country-info'),
-};
-
-const url = 'https://restcountries.com/v3.1/name/';
-
-function fetchCountries(name) {
-  return fetch(
-    `${url}${name}?fields=name,capital,population,flags,languages`
-  ).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
-}
+const refs = getRefs();
 
 refs.inputCountry.addEventListener(
   'input',
   debounce(onSearchCountry, DEBOUNCE_DELAY)
 );
-
 
 function onSearchCountry(e) {
    let name = e.target.value.trim();
@@ -37,7 +21,7 @@ function onSearchCountry(e) {
       return
    }
 
-  fetchCountries(name)
+  API.fetchCountries(name)
      .then(data => {
         if (data.length > 10) {
           return Notiflix.Notify.info(
@@ -59,34 +43,29 @@ function onSearchCountry(e) {
     );
 };
 
-
 function createMarkupCountry(countryArray) {
-   clearInputSearch();
-   const { name, capital, population, languages } = countryArray[0];
-   console.log({ name, capital, population, languages });
-  return `<div><img src = "${
-    countryArray[0].flags.svg
+ clearInputSearch();
+  const { name, capital, population, languages } = countryArray[0];
+  console.log({ name, capital, population, languages });
+  return `<div class="country-info_wrap" ><img src = "${
+    countryArray[0].flags.png
   }" alt = "country flag" width = "50">
-  <span><strong>${
-    name.official
-  }</strong></span></div>
-  <div ><strong>Capital:</strong> ${capital.join(
-    ', '
-  )}</div>
+  <span><strong>${name.official}</strong></span></div>
+  <div ><strong>Capital:</strong> ${capital.join(', ')}</div>
   <div><strong>Population:</strong> ${population}</div>
-  <div><strong>Languages:</strong> ${Object.values(
-    languages
-  )}</div>
+  <div><strong>Languages:</strong> ${Object.values(languages)}</div>
   `;
-};
+}
 
 function createMarkupCountryList(countries) {
-   clearInputSearch();
-   return countries.map(country => {
-      return `<li class="count_list"><img src = "${country.flags.svg}" alt = "country flag" width="50">
-  <span><strong>${country.name.common}</strong></span></li>`
-   }).join("");
-};
+clearInputSearch();
+  return countries
+    .map(country => {
+      return `<li><div class="country-list_wrap"><img src = "${country.flags.png}" alt = "country flag" width="50">
+  <span><strong>${country.name.common}</strong></span></div></li>`;
+    })
+    .join('');
+}
 
 function clearInputSearch() {
    refs.countryInfo.innerHTML = "";
